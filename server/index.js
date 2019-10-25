97b40be8a7dfbfcfe25cca768aee6b09ca1699f4
grayluker05 <graysonluker@gmail.com>
2019-10-25 16:19:29 -0600

    last commit

--- server/index.js
+++ server/index.js
@@ -2,9 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const massive = require('massive');
 const {SERVER_PORT, CONNECTION_STRING} = process.env;
-const ctrl = require('./controller');
 const app = express();
-
 app.use = (express.json());
 
 massive(CONNECTION_STRING).then(db => {
--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {updateUser} from '../../ducks/reducer';
 import axios from 'axios';
 
 class Auth extends Component {
@@ -17,29 +16,6 @@ class Auth extends Component {
         })
     }
 
-    handleRegister = () => {
-        axios.post('/auth/register', {username: this.state.username, password: this.state.password}).then(res => {
-            this.setState({
-                username: '',
-                password: ''
-            })
-            this.props.updateUser(res.data)
-            this.props.history.push('/dashboard')
-        })
-        .catch(err => console.log(err))
-    }
-
-    handleLogin = () => {
-        axios.post('/auth/login', {username: this.state.username, password: this.state.password}).then(res => {
-            this.setState({
-                username: '',
-                password: ''
-            })
-            this.props.updateUser(res.data)
-            this.props.history.push('/dashboard')
-        })
-    }
-
     render(){
         return(
             <div>
@@ -52,8 +28,8 @@ class Auth extends Component {
                 type='password'
                 name='password'
                 onChange={(e) => this.handleInput(e)}/>
-                <button onChange={(e) => this.handleLogin(e)}>Login</button>
-                <button onChange={(e) => this.handleRegister(e)}>Register</button>
+                <button>Login</button>
+                <button>Register</button>
             </div>
         )
     }
